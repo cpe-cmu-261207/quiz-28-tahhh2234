dd71e196ea2a49bcfdbc73955f0bda1eec07eb2c
tahhh <thadpongsermsook1@gmail.com>
2022-08-04 14:11:09 +0700

    hehe

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const btnRandom = document.getElementById("btn-random");
+const imgRandom = document.getElementById("div-user-card");
+const pname = document.getElementById("p-name");
+const gender = document.getElementById("span-gender-icon");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  alert(resp.data.results[0].picture.large);
+  imgRandom.src = resp.data.results[0].picture.large;
+  pname = resp.data.results[0].name.first;
+  if (resp.data.results[0].gender == male) gender = "👨";
+  if (resp.data.results[0].gender == female) gender = "👩";
 }
+
+btnRandom.onclick = async () => {
+  callApi();
+};
